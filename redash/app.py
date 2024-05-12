9d3e44fde98bdc774f4b54aae8696963e01d7b7a
Hillary Kipkemoi <tenskap@gmail.com>
2024-05-12 06:23:57 +0300

    change the endpoint ot match the api endpoint

--- redash/app.py
+++ redash/app.py
@@ -17,7 +17,8 @@ class Redash(Flask):
         )
         super(Redash, self).__init__(__name__, *args, **kwargs)
         # Make sure we get the right referral address even behind proxies like nginx.
-        self.wsgi_app = ProxyFix(self.wsgi_app, x_for=settings.PROXIES_COUNT, x_host=1)
+        # Set the number of to 2, for quart server and the redash server defined in nginx
+        self.wsgi_app = ProxyFix(self.wsgi_app, x_for=2, x_host=2)
         # Configure Redash using our settings
         self.config.from_object("redash.settings")
 
