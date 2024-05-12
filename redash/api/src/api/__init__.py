d83bdefa4cb04d3690623e342640ee58dee02ad4
Hillary Kipkemoi <tenskap@gmail.com>
2024-05-12 17:13:36 +0300

    Get the answer from llm working with langchain ✅

--- redash/api/src/api/__init__.py
+++ redash/api/src/api/__init__.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import os
 from openai import OpenAI
 import logging
-from utils.utils import get_schema
+from utils.utils import get_schema, get_llm_response
 
 app = Quart(__name__)
 
@@ -29,19 +29,15 @@ async def echo():
 @app.route('/api/chat', methods=['POST'])
 async def handle_user_question():
     try:
-        schema = await get_schema()
+        schema = get_schema()
         logging.info(schema)
+
+
         value = await request.get_json()
         question = value.get('question')
-        completion = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system",
-                 "content": "You are a redash visualization assistant, skilled in SQL queries and data visualization. You are only required to give answers for query and data visualization questions. If asked about a topic outside these two, make sure to respond that you have no information regarding that question. I am only here to help you with your query and data visualization questions. When asked to write queries, only provide the code without descriptions."},
-                {"role": "user", "content": question}
-            ]
-        )
-        answer = completion.choices[0].message.content
+
+        answer = get_llm_response(question)
+
         response_data = {"answer": answer}
         return jsonify(response_data), 200
     except Exception as error:
--- redash/api/src/utils/utils.py
+++ redash/api/src/utils/utils.py
@@ -1,4 +1,7 @@
 import psycopg2
+import os
+from langchain_openai import OpenAI
+from langchain_core.prompts import SystemMessagePromptTemplate
 
 # Function to get the schema from the database
 # TODO - make it reusable by having the table connection details and table name as parameters
@@ -37,3 +40,44 @@ def get_schema():
         schema += f"- {table_name}: Contains information about the {table_name}. Columns: {', '.join(columns)}.\n"
 
     return schema
+
+def filter_llm_answer(answer: str) -> str:
+    # Find the index of "System:"
+    system_index = answer.find("System:")
+
+    # Get everything after "System:"
+    sql_statement = answer[system_index + len("System:"):].strip()
+
+    return sql_statement
+
+def get_llm_response(question: str) -> str:
+    api_key = os.environ.get("OPENAI_API_KEY")
+
+    # Initialize the OpenAI object
+    llm = OpenAI(openai_api_key=api_key)
+
+    # Get the schema of the youtube_data database
+    schema = get_schema()
+
+    sql_query_example = "SELECT 'date', 'Content_type_Videos', 'Device type_Mobile phone' FROM youtube_chart_data"
+
+    # Define the system message
+    system_message = (f"You are a nice assistant. You are trained to generate SQL queries for Redash based on user's questions. "
+                      f"An example is this: {sql_query_example}. But it must note be exact like that. "
+                      f"Just note that the table names are enclosed in double quotations and the part after 'FROM'. \n"
+                      f"You are not to make up any information, if you don't know, say 'I don't know'. "
+                      f"The date field has values in this format: YYYY-MM-DD. "
+                      f"You have access to the youtube database with the following schema:\n") + schema
+
+    # Create a SystemMessagePromptTemplate from the system message
+    prompt = (
+        SystemMessagePromptTemplate.from_template(system_message) + "{question}"
+    )
+
+    # Chain the prompt and the OpenAI object together
+    llm_chain = prompt | llm
+
+    # Invoke the chain to get a response from the model
+    answer = llm_chain.invoke(question)
+    answer = filter_llm_answer(answer)
+    return answer
